19f505400c79f88c82dd55f21e20efc45b0fff4c
Brent Boghosian <brent.boghosian@remote-learner.net>
2015-03-17 11:07:46 -0400

    ELIS-9083: Update version releases & build dates.

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2015010401;
-$plugin->release = '2.8.1.0 (Build: 20150104)';
+$plugin->release = '2.8.5.0 (Build: 20150317)';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
     'local_elisprogram' => 2015010401
