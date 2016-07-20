9e674dbdac41e1eed228f84b6264bca21990b37c
Brent Boghosian <brent.boghosian@remote-learner.net>
2016-07-20 13:59:47 -0400

    ELIS-9410: Initial ELIS 3.1 version.

--- version.php
+++ version.php
@@ -25,10 +25,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2015102200;
-$plugin->release = '3.0.0.0 (Build: 20151022)';
+$plugin->version = 2016052300;
+$plugin->release = '3.1.0.0 (Build: 20160720)';
 $plugin->component = 'block_courserequest';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
-    'local_elisprogram' => 2015102200
+    'local_elisprogram' => 2016052300
 );
